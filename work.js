67290d1aec0f6e728c6f75f769aa947ef79da38c
Rahul Kumar <rkkamat9213@gmail.com>
2022-12-04 21:00:21 +0530

    imp_code

---
+++ work.js
@@ -0,0 +1,20 @@
+//p1.   check whether 7 is prime number or not
+   let count=0;
+for(let i=1;i<=7;i++){
+if(i%7==0){
+count++
+}
+}
+
+
+
+
+//P2.   check whether "madam" is a palidrome or not.
+
+
+let str="madam"
+let bag=0;
+for(let i=0;i<str;i++){
+bag+=str[i]
+}
+console.log(bag)
